21b6228f4a616e8c4d17cd33e16bf1d2930b88ab
Zea-Zee <glebru.irk38@yandex.ru>
2024-02-15 15:03:43 +0700

    first cmake commit

--- 1stTask/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/zea/Desktop/Parallelism/cmakeQuickStart
-# Build directory: /home/zea/Desktop/Parallelism/cmakeQuickStart/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
